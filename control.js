84aa4787b572b2b71a5b5671c24850c639cf4ace
amruthasabu <amrutha136sabu@gmail.com>
2025-06-19 11:39:26 +0530

    first commit

---
+++ control.js
@@ -0,0 +1,41 @@
+//  //Ternary operator
+//  let x= 10;
+//  //x<11 failed otherwise pass
+//  //condition ?failed:pass
+  
+//    for(let i=0;i<10;i++){
+//      console.log(i);
+//    }
+// let cars=["bnw","audi","mercedes"];
+// //for in loop
+// for(let i=0;i<cars.length;i++){
+//     console.log(cars[i]);
+// }
+// // for each
+// cars.forEach(function(car){
+//     console.log(car);
+// });
+// let person={
+//     name:"John",age:30,city:"New York"
+// };
+// for(x in person){
+//     console.log(person[x]);
+// }
+// //while loop
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+// var j=0;
+// while(j<fruits.length){
+//     console.log(fruits[j]);
+//     j++;
+// }
+//do while loop
+let k=0;
+do{
+    console.log(k);
+    k++;
+}while(k<10);   
+
